ef9b60f42a29c797659f1457ea682ccdf0a5ae9a
eneko-osia <eneko.osia@gmail.com>
2020-02-23 13:36:35 +0100

    [core] fix linux failure

--- src/core/test/gg/core/pattern/non_copyable_test.cpp
+++ src/core/test/gg/core/pattern/non_copyable_test.cpp
@@ -43,7 +43,7 @@ TEST_CASE("non_copyable", "[gg.non_copyable]")
     SECTION("pod")
     {
 #if defined(GG_LINUX)
-        REQUIRE(type::is_pod<non_copyable>::value;
+        REQUIRE(type::is_pod<non_copyable>::value);
 #elif defined(GG_WINDOWS)
         REQUIRE(!type::is_pod<non_copyable>::value);
 #endif
