4dcf5bff98605252d5f5362febb9343ab840329b
eneko-osia <eneko.osia@gmail.com>
2020-05-24 10:25:33 +0200

    [app] windows console updated

--- src/app/src/gg/app/debug/console/console.cpp
+++ src/app/src/gg/app/debug/console/console.cpp
@@ -6,12 +6,8 @@
 //==============================================================================
 
 #include "gg/app/application/application.h"
-#include "gg/app/data/data.h"
-#include <fcntl.h>
-#include <fstream>
-#include <io.h>
+#include <cstdio>
 #include <iostream>
-#include <stdio.h>
 #include <windows.h>
 
 //==============================================================================
@@ -19,61 +15,28 @@ namespace gg::console
 {
 //==============================================================================
 
-static void handle_messages(uint32 fdw_ctrl_type)
-{
-    switch (fdw_ctrl_type)
-    {
-        case CTRL_CLOSE_EVENT:
-        case CTRL_C_EVENT:
-        {
-            // application::get_instance().on_exit(nullptr);
-            break;
-        }
-
-        default:
-        {
-            break;
-        }
-    }
-}
-
-//==============================================================================
-
 void finalize(void) noexcept
 {
+    GG_RETURN_IF_NULL(GetConsoleWindow());
+
+    GG_RETURN_IF_NULL(freopen("NUL:", "w", stderr));
+    GG_RETURN_IF_NULL(freopen("NUL:", "w", stdout));
+    GG_RETURN_IF_NULL(freopen("NUL:", "r", stdin));
+    GG_RETURN_IF_TRUE(FALSE == FreeConsole());
 }
 
 bool8 init(void) noexcept
 {
-    // check if there is not already a console window
+    GG_RETURN_TRUE_IF_NOT_NULL(GetConsoleWindow());
 
-    GG_RETURN_TRUE_IF_TRUE_ASSERT(GetConsoleWindow());
-
-    // allocate console
-
-    GG_RETURN_FALSE_IF_FALSE_ASSERT(AllocConsole());
-
-    // set console title
-
-    SetConsoleTitle(application::get_instance().get_name().begin());
-
-    // attach console
-
-    AttachConsole(GetCurrentProcessId());
-
-    // stdout handler
-
-    GG_RETURN_FALSE_IF_FALSE_ASSERT(freopen("CONOUT$", "w", stdout));
-
-    // set console handler
-
-    GG_RETURN_FALSE_IF_FALSE_ASSERT(
-        SetConsoleCtrlHandler((PHANDLER_ROUTINE) handle_messages, true));
-
-    // sync with stdio
-
-    std::ios::sync_with_stdio();
-
+    GG_RETURN_FALSE_IF_TRUE(FALSE == AllocConsole());
+    GG_RETURN_FALSE_IF_TRUE(
+        FALSE == SetConsoleTitle(
+            application::get_instance().get_name().begin()));
+    GG_RETURN_FALSE_IF_NULL(freopen("CONIN$", "r", stdin));
+    GG_RETURN_FALSE_IF_NULL(freopen("CONOUT$", "w", stdout));
+    GG_RETURN_FALSE_IF_NULL(freopen("CONOUT$", "w", stderr));
+    GG_RETURN_FALSE_IF_FALSE(std::ios::sync_with_stdio());
     return true;
 }
 
