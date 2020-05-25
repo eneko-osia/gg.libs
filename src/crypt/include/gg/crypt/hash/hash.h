02f636e604ce1d2a658e46f50fa40781a9e498b4
eneko-osia <eneko.osia@gmail.com>
2020-05-25 15:32:04 +0200

    [crypt] hash constexpr

--- src/crypt/include/gg/crypt/hash/hash.h
+++ src/crypt/include/gg/crypt/hash/hash.h
@@ -3,7 +3,7 @@
 
 // include files
 
-#include "gg/core/type/type.h"
+#include "gg/core/string/type/string_ref.h"
 
 // namespace
 
@@ -23,7 +23,19 @@ namespace gg
         {
             // methods
 
-            uint32 generate(string_ref const & string);
+            inline constexpr uint32 generate(string_ref const & string)
+            {
+                static constexpr uint32 k_offset_basis = 0x811c9dc5;
+                static constexpr uint32 k_prime = (1 << 24) + (1 << 8) + 0x93;
+
+                uint32 hash = k_offset_basis;
+                for (auto cit = string.begin(); cit != string.end(); ++cit)
+                {
+                    hash *= k_prime;
+                    hash ^= type::cast_static<uint8>(*cit);
+                }
+                return hash;
+            }
         }
 
         // namespace in charge of define fnv1a hash utilities
@@ -32,7 +44,19 @@ namespace gg
         {
             // methods
 
-            uint32 generate(string_ref const & string);
+            inline constexpr uint32 generate(string_ref const & string)
+            {
+                static constexpr uint32 k_offset_basis = 0x811c9dc5;
+                static constexpr uint32 k_prime = (1 << 24) + (1 << 8) + 0x93;
+
+                uint32 hash = k_offset_basis;
+                for (auto cit = string.begin(); cit != string.end(); ++cit)
+                {
+                    hash ^= type::cast_static<uint8>(*cit);
+                    hash *= k_prime;
+                }
+                return hash;
+            }
         }
     }
 }
