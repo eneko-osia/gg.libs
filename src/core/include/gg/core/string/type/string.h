e7b6e97e689fe9ea89d033d618690c0753e6604d
eneko-osia <eneko.osia@gmail.com>
2020-05-25 15:27:52 +0200

    [core] remove cstring dependency

--- src/core/include/gg/core/string/type/string.h
+++ src/core/include/gg/core/string/type/string.h
@@ -4,9 +4,8 @@
 // include files
 
 #include "gg/core/constant/constant.h"
+#include "gg/core/debug/assert.h"
 #include "gg/core/string/macro/macro.h"
-#include "gg/core/type/type_trait.h"
-#include <cstring>
 
 // namespace
 
@@ -22,55 +21,68 @@ namespace gg
 
         // methods
 
-        inline diff_type
+        inline constexpr diff_type
         compare(char8 const * lhs, char8 const * rhs) noexcept
         {
             GG_ASSERT_NOT_NULL(lhs);
             GG_ASSERT_NOT_NULL(rhs);
-            return std::strcmp(lhs, rhs);
+
+            for (; (*lhs && *rhs) && (*lhs == *rhs); ++lhs, ++rhs) {}
+            return *(uchar8 const *) lhs - *(uchar8 const *) rhs;
         }
 
-        inline char8 const *
+        inline constexpr char8 const *
         find(char8 const * lhs, char8 const * rhs) noexcept
         {
             GG_ASSERT_NOT_NULL(lhs);
             GG_ASSERT_NOT_NULL(rhs);
-            return std::strstr(lhs, rhs);
+
+            for (; *lhs; ++lhs)
+            {
+                char8 const * j = rhs;
+                for (char8 const * i = lhs; (*i && *j) && (*i == *j); ++i, ++j)
+                {
+                }
+
+                if (!(*j))
+                {
+                    return lhs;
+                }
+            }
+            return nullptr;
         }
 
-        inline size_type length(char8 const * str) noexcept
+        inline constexpr size_type length(char8 const * str) noexcept
         {
             GG_ASSERT_NOT_NULL(str);
-            return type::cast_static<size_type>(std::strlen(str));
+
+            size_type size = 0;
+            for (; *str; ++str, ++size) {}
+            return size;
         }
 
-        inline void trim(
+        inline constexpr void trim(
             char8 * str,
             size_type size,
             char8 const * delims = GG_TEXT(" \t\r\n")) noexcept
         {
-            bool8 delim = false;
-            size_type index = 0;
+            GG_ASSERT_NOT_NULL(str);
+            GG_ASSERT_NOT_NULL(delims);
 
             char8 const * str_end = str + size;
             char8 const * delims_end = delims + length(delims);
-
-            for(char8 const * i = str; i != str_end; ++i)
+            for(char8 const * i = delims; i != delims_end; ++i)
             {
-                for(char8 const * j = delims; j != delims_end && !delim; ++j)
+                size_type index = 0;
+                for(char8 const * j = str; j != str_end; ++j)
                 {
-                    if (*i == *j) { delim = true; }
+                    if (*i != *j)
+                    {
+                        str[index++] = (*j);
+                    }
                 }
-
-                if (!delim)
-                {
-                    str[index++] = (*i);
-                }
-
-                delim = false;
+                str[index] = '\0';
             }
-
-            str[index] = '\0';
         }
     }
 }
