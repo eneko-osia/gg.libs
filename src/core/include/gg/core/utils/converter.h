b3fa756caf75f179bc6affa8de7251d8bd52f9e9
eneko-osia <eneko.osia@gmail.com>
2020-06-10 23:31:32 +0200

    [core] converter updated

--- src/core/include/gg/core/utils/converter.h
+++ src/core/include/gg/core/utils/converter.h
@@ -1,14 +1,9 @@
 #ifndef _gg_converter_h_
 #define	_gg_converter_h_
 
-// include files
-
 #include "gg/core/type/type_trait.h"
 #include "gg/core/utils/text_utils.h"
-
 #include <cmath>
-
-// namespace
 
 namespace gg
 {
@@ -19,120 +14,95 @@ namespace gg
         template <typename TYPE>
         TYPE to(char8 const * value) noexcept;
 
-        template<>
+        template <>
         inline bool8 to<bool8>(char8 const * value) noexcept
         {
             return type::cast_static<bool8>(0 != ::atoi(value));
         }
 
-        template<>
+        template <>
         inline int8 to<int8>(char8 const * value) noexcept
         {
             return type::cast_static<int8>(::atoi(value));
         }
 
-        template<>
-        inline float32 to<float32>(char8 const * value) noexcept
-        {
-            return type::cast_static<float32>(::atof(value));
-        }
-
-        template<>
-        inline int32 to<int32>(char8 const * value) noexcept
-        {
-            return type::cast_static<int32>(::atoi(value));
-        }
-
-        template<>
-        inline int64 to<int64>(char8 const * value) noexcept
-        {
-            return type::cast_static<int64>(::atol(value));
-        }
-
-        template<>
+        template <>
         inline int16 to<int16>(char8 const * value) noexcept
         {
             return type::cast_static<int16>(::atoi(value));
         }
 
-        template<>
+        template <>
+        inline int32 to<int32>(char8 const * value) noexcept
+        {
+            return type::cast_static<int32>(::atoi(value));
+        }
+
+        template <>
+        inline int64 to<int64>(char8 const * value) noexcept
+        {
+            return type::cast_static<int64>(::atol(value));
+        }
+
+        template <>
         inline uint8 to<uint8>(char8 const * value) noexcept
         {
             return type::cast_static<uint8>(::atoi(value));
         }
 
-        template<>
+        template <>
+        inline uint16 to<uint16>(char8 const * value) noexcept
+        {
+            return type::cast_static<uint16>(::atoi(value));
+        }
+
+        template <>
         inline uint32 to<uint32>(char8 const * value) noexcept
         {
             return type::cast_static<uint32>(::atoi(value));
         }
 
-        template<>
+        template <>
         inline uint64 to<uint64>(char8 const * value) noexcept
         {
             return type::cast_static<uint64>(::atol(value));
         }
 
-        template<>
-        inline uint16 to<uint16>(char8 const * value) noexcept
+        template <>
+        inline float32 to<float32>(char8 const * value) noexcept
         {
-            return type::cast_static<uint16>(::atoi(value));
+            return type::cast_static<float32>(::atof(value));
+        }
+
+        template <>
+        inline float64 to<float64>(char8 const * value) noexcept
+        {
+            return type::cast_static<float64>(::atof(value));
+        }
+
+        template <typename TYPE, typename FROM>
+        TYPE to(FROM value) noexcept;
+
+        template <>
+        inline int32 to<int32, float32>(float32 value) noexcept
+        {
+            return
+                type::cast_static<int32>(
+                    value + ((fmodf(value, 1.0f) < 0.5f) ? 0 : 1));
+        }
+
+        template <>
+        inline uint32 to<uint32, float32>(float32 value) noexcept
+        {
+            return
+                type::cast_static<uint32>(
+                    value + ((fmodf(value, 1.0f) < 0.5f) ? 0 : 1));
         }
     }
 
     namespace converter
     {
         // methods
-
-        inline bool8 atob(char8 const * value) noexcept
-        {
-            return type::cast_static<bool8>(0 != atoi(value));
-        }
-
-        inline int8 atoc(char8 const * value) noexcept
-        {
-            return type::cast_static<int8>(::atoi(value));
-        }
-
-        inline float32 atof(char8 const * value) noexcept
-        {
-            return type::cast_static<float32>(::atof(value));
-        }
-
-        inline int32 atoi(char8 const * value) noexcept
-        {
-            return type::cast_static<int32>(::atoi(value));
-        }
-
-        inline int64 atol(char8 const * value) noexcept
-        {
-            return type::cast_static<int64>(::atol(value));
-        }
-
-        inline int16 atos(char8 const * value) noexcept
-        {
-            return type::cast_static<int16>(atoi(value));
-        }
-
-        inline uint8 atouc(char8 const * value) noexcept
-        {
-            return type::cast_static<uint8>(::atoi(value));
-        }
-
-        inline uint32 atoui(char8 const * value) noexcept
-        {
-            return type::cast_static<uint32>(::atoi(value));
-        }
-
-        inline uint64 atoul(char8 const * value) noexcept
-        {
-            return type::cast_static<uint64>(::atol(value));
-        }
-
-        inline uint16 atous(char8 const * value) noexcept
-        {
-            return type::cast_static<uint16>(atoui(value));
-        }
 
         inline void btoa(bool8 b, char8 * buffer, uint32 size) noexcept
         {
@@ -142,16 +112,6 @@ namespace gg
         inline void ftoa(float32 f, char8 * buffer, uint32 size) noexcept
         {
             text_utils::format(buffer, size, "%f", f);
-        }
-
-        inline int32 ftoi(float32 f) noexcept
-        {
-            return type::cast_static<int32>(f + ((fmodf(f, 1.0f) < 0.5f) ? 0 : 1));
-        }
-
-        inline uint32 ftou(float32 f) noexcept
-        {
-            return type::cast_static<uint32>(f + ((fmodf(f, 1.0f) < 0.5f) ? 0 : 1));
         }
 
         inline void itoa(int32 i, char8 * buffer, uint32 size) noexcept
