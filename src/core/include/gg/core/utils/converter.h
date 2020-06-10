df597d1e652e37e33cac9bcafb72c69f33db2305
eneko-osia <eneko.osia@gmail.com>
2020-06-10 19:41:06 +0200

    [core] add converter templated methods

--- src/core/include/gg/core/utils/converter.h
+++ src/core/include/gg/core/utils/converter.h
@@ -12,7 +12,73 @@
 
 namespace gg
 {
-    // namespace in charge of realize conversions between different types
+    namespace convert
+    {
+        // methods
+
+        template <typename TYPE>
+        TYPE to(char8 const * value) noexcept;
+
+        template<>
+        inline bool8 to<bool8>(char8 const * value) noexcept
+        {
+            return type::cast_static<bool8>(0 != ::atoi(value));
+        }
+
+        template<>
+        inline int8 to<int8>(char8 const * value) noexcept
+        {
+            return type::cast_static<int8>(::atoi(value));
+        }
+
+        template<>
+        inline float32 to<float32>(char8 const * value) noexcept
+        {
+            return type::cast_static<float32>(::atof(value));
+        }
+
+        template<>
+        inline int32 to<int32>(char8 const * value) noexcept
+        {
+            return type::cast_static<int32>(::atoi(value));
+        }
+
+        template<>
+        inline int64 to<int64>(char8 const * value) noexcept
+        {
+            return type::cast_static<int64>(::atol(value));
+        }
+
+        template<>
+        inline int16 to<int16>(char8 const * value) noexcept
+        {
+            return type::cast_static<int16>(::atoi(value));
+        }
+
+        template<>
+        inline uint8 to<uint8>(char8 const * value) noexcept
+        {
+            return type::cast_static<uint8>(::atoi(value));
+        }
+
+        template<>
+        inline uint32 to<uint32>(char8 const * value) noexcept
+        {
+            return type::cast_static<uint32>(::atoi(value));
+        }
+
+        template<>
+        inline uint64 to<uint64>(char8 const * value) noexcept
+        {
+            return type::cast_static<uint64>(::atol(value));
+        }
+
+        template<>
+        inline uint16 to<uint16>(char8 const * value) noexcept
+        {
+            return type::cast_static<uint16>(::atoi(value));
+        }
+    }
 
     namespace converter
     {
