5511efab2556cad818b8f8ec30333c27100e1490
eneko-osia <eneko.osia@gmail.com>
2020-06-14 10:03:55 +0200

    [core] memory tests updated

--- src/core/test/gg/core/memory/memory_test.cpp
+++ src/core/test/gg/core/memory/memory_test.cpp
@@ -10,6 +10,15 @@ namespace gg::memory_test
 {
 //==============================================================================
 
+struct mock_item
+{
+    mock_item(int32 value = 0) noexcept : m_value(value) {}
+    ~mock_item(void) noexcept = default;
+    int32 m_value;
+};
+
+//==============================================================================
+
 TEST_CASE("memory", "[gg.memory]")
 {
 #if defined(GG_DEBUG)
@@ -122,7 +131,8 @@ TEST_CASE("memory.allocate", "[gg.memory]")
 {
     SECTION("allocate")
     {
-        void * ptr = memory::allocate(16);
+        static uint32 constexpr k_bytes = 16;
+        void * ptr = memory::allocate(k_bytes);
         REQUIRE(ptr);
         memory::deallocate(ptr);
     }
@@ -141,13 +151,69 @@ TEST_CASE("memory.reallocate", "[gg.memory]")
 {
     SECTION("reallocate")
     {
+        static uint32 constexpr k_bytes = 16;
         void * ptr = nullptr;
-        ptr = memory::reallocate(ptr, 16);
+        ptr = memory::reallocate(ptr, k_bytes);
         REQUIRE(ptr);
         memory::deallocate(ptr);
     }
 }
 
+TEST_CASE("memory.construct_object", "[gg.memory]")
+{
+    SECTION("construct_object")
+    {
+        static uint32 constexpr k_value = 8;
+        mock_item * object = memory::allocate<mock_item>();
+        memory::construct_object(object, k_value);
+        REQUIRE(object->m_value == k_value);
+        object->~mock_item();
+        memory::deallocate(object);
+    }
+}
+
+TEST_CASE("memory.delete_object", "[gg.memory]")
+{
+    SECTION("delete_object")
+    {
+        mock_item * object = nullptr;
+        memory::delete_object(object);
+    }
+}
+
+TEST_CASE("memory.delete_object_array", "[gg.memory]")
+{
+    SECTION("delete_object_array")
+    {
+        mock_item * object = nullptr;
+        memory::delete_object_array(object);
+    }
+}
+
+TEST_CASE("memory.new_object", "[gg.memory]")
+{
+    SECTION("new_object")
+    {
+        static uint32 constexpr k_value = 8;
+        mock_item * object = memory::new_object<mock_item>(k_value);
+        REQUIRE(object->m_value == k_value);
+        memory::delete_object(object);
+    }
+}
+
+TEST_CASE("memory.new_object_array", "[gg.memory]")
+{
+    SECTION("new_object_array")
+    {
+        static uint32 constexpr k_max_num = 8;
+        mock_item * object = memory::new_object_array<mock_item>(k_max_num);
+        for (uint32 i = 0; i < k_max_num; ++i)
+        {
+            REQUIRE(object[i].m_value == 0);
+        }
+        memory::delete_object_array(object);
+    }
+}
 
 //==============================================================================
 }
