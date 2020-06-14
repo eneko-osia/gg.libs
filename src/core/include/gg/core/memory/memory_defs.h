8791d27e5ee5c5575ce9fe032d70e794b45e4fef
eneko-osia <eneko.osia@gmail.com>
2020-06-14 10:46:22 +0200

    [core] memory format

--- src/core/include/gg/core/memory/memory_defs.h
+++ src/core/include/gg/core/memory/memory_defs.h
@@ -1,17 +1,11 @@
 #ifndef _gg_memory_defs_h_
 #define _gg_memory_defs_h_
 
-// include files
-
 #include "gg/core/string/type/string_ref.h"
 #include "gg/core/type/enum.h"
 
-// namespace
-
 namespace gg
 {
-    // enumeration
-
     enum class memory_owner_mode : uint8
     {
         is_not_owner,
@@ -22,14 +16,10 @@ namespace gg
     GG_ENUM(memory_owner_mode);
 
 #if defined(GG_DEBUG)
-    // enumeration descriptors
-
     static constexpr string_ref memory_owner_mode_descriptors[] = {
         GG_TEXT("is_not_owner"),
         GG_TEXT("is_owner")
     };
-
-    // static assert
 
     GG_ASSERT_STATIC(
         enums::cast(memory_owner_mode::max) ==
