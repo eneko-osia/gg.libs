29bdb3b6fd4c7e01f7048d1eab14d180e042187a
eneko-osia <eneko.osia@gmail.com>
2020-07-06 22:51:57 +0200

    [core] memory istream added

---
+++ src/core/include/gg/core/stream/memory_istream.h
@@ -0,0 +1,38 @@
+#ifndef _gg_memory_istream_h_
+#define _gg_memory_istream_h_
+
+// include files
+
+#include "gg/core/stream/istream.h"
+
+namespace gg
+{
+    class memory_buffer_ref;
+
+    class memory_istream final : public istream<memory_istream>
+    {
+    public:
+
+        // constructors
+
+        memory_istream(
+            memory_buffer_ref const & buffer,
+            endian_mode endian_mode = endian::system_mode) noexcept;
+        ~memory_istream(void) noexcept = default;
+
+        // methods
+
+        bool8 move(size_type position) noexcept;
+        size_type read(void * buffer, size_type bytes) noexcept;
+        size_type read_line(char8 * buffer, size_type max_size) noexcept;
+
+    private:
+
+        // attributes
+
+        memory_buffer_ref const & m_buffer;
+        size_type m_position;
+    };
+}
+
+#endif // _gg_memory_istream_h_
---
+++ src/core/src/gg/core/stream/memory_istream.cpp
@@ -0,0 +1,56 @@
+#include "gg/core/stream/memory_istream.h"
+
+//==============================================================================
+
+#include "gg/core/memory/memory_buffer_ref.h"
+#include "gg/core/memory/memory.h"
+
+//==============================================================================
+namespace gg
+{
+//==============================================================================
+
+memory_istream::memory_istream(
+    memory_buffer_ref const & buffer,
+    endian_mode endian_mode) noexcept
+    : istream(endian_mode)
+    , m_buffer(buffer)
+    , m_position(0)
+{
+}
+
+//==============================================================================
+
+bool8 memory_istream::move(size_type position) noexcept
+{
+    GG_ASSERT(position <= m_buffer.size());
+    m_position = position;
+    return true;
+}
+
+size_type memory_istream::read(void * buffer, size_type size) noexcept
+{
+    size_type read_size =
+        ((m_position + size) > m_buffer.size()) ?
+            m_buffer.size() - m_position : size;
+    memory::copy(buffer, &m_buffer.get(m_position), read_size);
+    m_position += read_size;
+    return read_size;
+}
+
+size_type memory_istream::read_line(char8 * buffer, size_type max_size) noexcept
+{
+    for (size_type i = m_position; i < m_buffer.size(); ++i)
+    {
+        if ('\n' == m_buffer.get<char8>(i))
+        {
+            size_type read_size = i - m_position;
+            return read(buffer, (read_size > max_size) ? max_size : read_size);
+        }
+    }
+    return 0;
+}
+
+//==============================================================================
+}
+//==============================================================================
