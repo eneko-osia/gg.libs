8eb14455263075bd06111e1a994e4a2ed4164d5f
eneko-osia <eneko.osia@gmail.com>
2020-07-11 11:49:23 +0200

    [core] fix linux compilation

--- src/core/test/gg/core/stream/memory_istream_test.cpp
+++ src/core/test/gg/core/stream/memory_istream_test.cpp
@@ -56,13 +56,13 @@ TEST_CASE("memory_istream.constructor", "[gg.memory_istream]")
 {
     SECTION("memory_istream(buffer)")
     {
-        char8 * string = "This is a buffer";
-        memory_buffer_ref buffer(
-            type::cast_reinterpret<byte *>(string), string::length(string));
+        char8 data[16];
+        memory::copy(data, "This is a buffer", sizeof(data));
+        memory_buffer_ref buffer((byte *) (data), sizeof(data));
         memory_istream stream(buffer);
         REQUIRE(stream.is_valid());
         REQUIRE(stream.get_position() == 0);
-        REQUIRE(stream.size() == string::length(string));
+        REQUIRE(stream.size() == sizeof(data));
     }
 
     SECTION("memory_istream(invalid buffer)")
@@ -79,13 +79,13 @@ TEST_CASE("memory_istream.move", "[gg.memory_istream]")
 {
     SECTION("move")
     {
-        char8 * string = "This is a buffer";
-        memory_buffer_ref buffer(
-            type::cast_reinterpret<byte *>(string), string::length(string));
+        char8 data[16];
+        memory::copy(data, "This is a buffer", sizeof(data));
+        memory_buffer_ref buffer((byte *) (data), sizeof(data));
         memory_istream stream(buffer);
         REQUIRE(stream.move(10));
         REQUIRE(stream.get_position() == 10);
-        REQUIRE(stream.size() == string::length(string));
+        REQUIRE(stream.size() == sizeof(data));
     }
 }
 
@@ -93,16 +93,16 @@ TEST_CASE("memory_istream.read", "[gg.memory_istream]")
 {
     SECTION("read")
     {
-        char8 * string = "This is a buffer";
-        memory_buffer_ref buffer(
-            type::cast_reinterpret<byte *>(string), string::length(string));
+        char8 data[16];
+        memory::copy(data, "This is a buffer", sizeof(data));
+        memory_buffer_ref buffer((byte *) (data), sizeof(data));
         memory_istream stream(buffer);
 
         char8 character = '\0';
         REQUIRE(sizeof(char8) == stream.read(character));
         REQUIRE(character == 'T');
         REQUIRE(stream.get_position() == sizeof(char8));
-        REQUIRE(stream.size() == string::length(string));
+        REQUIRE(stream.size() == sizeof(data));
     }
 }
 
@@ -110,43 +110,43 @@ TEST_CASE("memory_istream.read_line", "[gg.memory_istream]")
 {
     SECTION("read_line - one line")
     {
-        char8 * string = "This is a buffer";
-        memory_buffer_ref buffer(
-            type::cast_reinterpret<byte *>(string), string::length(string));
+        char8 data[16];
+        memory::copy(data, "This is a buffer", sizeof(data));
+        memory_buffer_ref buffer((byte *) (data), sizeof(data));
         memory_istream stream(buffer);
 
         string_static<32> line;
         REQUIRE(stream.read_line(line.c_str(), line.max_size()));
-        REQUIRE(line == string);
-        REQUIRE(stream.get_position() == line.size());
-        REQUIRE(stream.size() == string::length(string));
+        REQUIRE(line == "This is a buffer");
+        REQUIRE(stream.get_position() == sizeof(data));
+        REQUIRE(stream.size() == sizeof(data));
 
         REQUIRE(!stream.read_line(line.c_str(), line.max_size()));
-        REQUIRE(stream.get_position() == string::length(string));
-        REQUIRE(stream.size() == string::length(string));
+        REQUIRE(stream.get_position() == sizeof(data));
+        REQUIRE(stream.size() == sizeof(data));
     }
 
     SECTION("read_line - multi line")
     {
-        char8 * string = "This is a buffer\nThis is another line";
-        memory_buffer_ref buffer(
-            type::cast_reinterpret<byte *>(string), string::length(string));
+        char8 data[37];
+        memory::copy(data, "This is a buffer\nThis is another line", sizeof(data));
+        memory_buffer_ref buffer((byte *) (data), sizeof(data));
         memory_istream stream(buffer);
 
         string_static<32> line;
         REQUIRE(stream.read_line(line.c_str(), line.max_size()));
         REQUIRE(line == "This is a buffer\n");
         REQUIRE(stream.get_position() == line.size());
-        REQUIRE(stream.size() == string::length(string));
+        REQUIRE(stream.size() == sizeof(data));
 
         REQUIRE(stream.read_line(line.c_str(), line.max_size()));
         REQUIRE(line == "This is another line");
-        REQUIRE(stream.get_position() == string::length(string));
-        REQUIRE(stream.size() == string::length(string));
+        REQUIRE(stream.get_position() == sizeof(data));
+        REQUIRE(stream.size() == sizeof(data));
 
         REQUIRE(!stream.read_line(line.c_str(), line.max_size()));
-        REQUIRE(stream.get_position() == string::length(string));
-        REQUIRE(stream.size() == string::length(string));
+        REQUIRE(stream.get_position() == sizeof(data));
+        REQUIRE(stream.size() == sizeof(data));
     }
 }
 
