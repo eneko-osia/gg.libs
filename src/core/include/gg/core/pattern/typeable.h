64508878ac316053b87a7c12b106ac75f90bfa12
eneko-osia <eneko.osia@gmail.com>
2020-11-14 22:06:11 +0100

    [core] typeable class added

---
+++ src/core/include/gg/core/pattern/typeable.h
@@ -0,0 +1,71 @@
+#ifndef _gg_typeable_h_
+#define _gg_typeable_h_
+
+#include "gg/core/type/type_trait.h"
+
+namespace gg
+{
+    template <typename ITEM_TYPE>
+    class typeable
+    {
+    public:
+
+        // item_type definitions
+
+        typedef ITEM_TYPE item_type;
+
+        // constructors
+
+        typeable(void) noexcept = default;
+        typeable(typeable const & obj) noexcept = default;
+        typeable(typeable && obj) noexcept = default;
+
+        typeable(item_type const & type) noexcept
+            : m_type(type)
+        {
+        }
+
+        typeable(item_type && type) noexcept
+            : m_type(type::move(type))
+        {
+        }
+
+        // accessors
+
+        item_type const & get_type(void) const noexcept
+        {
+            return m_type;
+        }
+
+        void set_type(item_type const & type) noexcept
+        {
+            m_type = type;
+        }
+
+        void set_type(item_type && type) noexcept
+        {
+            m_type = type::move(type);
+        }
+
+        // inquiries
+
+        bool8 is_type(item_type const & type) const noexcept
+        {
+            return m_type == type;
+        }
+
+    protected:
+
+        // constructors
+
+        ~typeable(void) noexcept = default;
+
+    private:
+
+        // attributes
+
+        item_type m_type;
+    };
+}
+
+#endif // _gg_typeable_h_
---
+++ src/core/test/gg/core/pattern/typeable_test.cpp
@@ -0,0 +1,288 @@
+#include "gg/test/catch.h"
+
+//==============================================================================
+
+#include "gg/core/pattern/typeable.h"
+
+//==============================================================================
+namespace gg::typeable_test
+{
+//==============================================================================
+
+class mock_type
+{
+public:
+
+    // constructors
+
+    mock_type(void) noexcept : m_type (0)
+    {
+    }
+
+    mock_type(uint32 type) noexcept : m_type(type)
+    {
+    }
+
+    mock_type(mock_type const & type) noexcept : m_type(type.m_type)
+    {
+    }
+
+    mock_type(mock_type && type) noexcept : m_type(type.m_type)
+    {
+        type.m_type = 0;
+    }
+
+    // operators
+
+    mock_type & operator=(mock_type const & type) noexcept
+    {
+        m_type = type.m_type;
+        return *this;
+    }
+
+    mock_type & operator=(mock_type && type) noexcept
+    {
+        *this = type;
+        type.m_type = 0;
+        return *this;
+    }
+
+    bool8 operator==(mock_type const & type) const noexcept
+    {
+        return m_type == type.m_type;
+    }
+
+private:
+
+    uint32 m_type;
+};
+
+class mock_typeable : public typeable<mock_type>
+{
+public:
+
+    mock_typeable(void) noexcept = default;
+    mock_typeable(mock_typeable const & obj) noexcept = default;
+    mock_typeable(mock_typeable && obj) noexcept = default;
+
+    mock_typeable(mock_type const & type) noexcept
+        : typeable<mock_type>(type)
+    {
+    }
+
+    mock_typeable(mock_type && type) noexcept
+        : typeable<mock_type>(type::move(type))
+    {
+    }
+};
+
+class trivial_mock_typeable : public typeable<uint32>
+{
+public:
+
+    trivial_mock_typeable(void) noexcept = default;
+    trivial_mock_typeable(trivial_mock_typeable const & obj) noexcept = default;
+    trivial_mock_typeable(trivial_mock_typeable && obj) noexcept = default;
+};
+
+//==============================================================================
+
+TEST_CASE("typeable", "[gg.typeable]")
+{
+    SECTION("assign")
+    {
+        REQUIRE(!type::is_assignable<typeable<mock_type>>::value);
+        REQUIRE(!type::has_trivial_assign<typeable<mock_type>>::value);
+
+        REQUIRE(!type::is_assignable<mock_typeable>::value);
+        REQUIRE(!type::has_trivial_assign<mock_typeable>::value);
+
+        REQUIRE(!type::is_assignable<typeable<uint32>>::value);
+        REQUIRE(!type::has_trivial_assign<typeable<uint32>>::value);
+
+        REQUIRE(!type::is_assignable<trivial_mock_typeable>::value);
+        REQUIRE(!type::has_trivial_assign<trivial_mock_typeable>::value);
+    }
+
+    SECTION("construct")
+    {
+        REQUIRE(!type::is_constructible<typeable<mock_type>>::value);
+        REQUIRE(!type::has_trivial_constructor<typeable<mock_type>>::value);
+
+        REQUIRE(type::is_constructible<mock_typeable>::value);
+        REQUIRE(!type::has_trivial_constructor<mock_typeable>::value);
+
+        REQUIRE(!type::is_constructible<typeable<uint32>>::value);
+        REQUIRE(!type::has_trivial_constructor<typeable<uint32>>::value);
+
+        REQUIRE(type::is_constructible<trivial_mock_typeable>::value);
+        REQUIRE(type::has_trivial_constructor<trivial_mock_typeable>::value);
+    }
+
+    SECTION("copy")
+    {
+        REQUIRE(!type::is_copyable<typeable<mock_type>>::value);
+        REQUIRE(!type::has_trivial_copy<typeable<mock_type>>::value);
+
+        REQUIRE(type::is_copyable<mock_typeable>::value);
+        REQUIRE(!type::has_trivial_copy<mock_typeable>::value);
+
+        REQUIRE(!type::is_copyable<typeable<uint32>>::value);
+        REQUIRE(!type::has_trivial_copy<typeable<uint32>>::value);
+
+        REQUIRE(type::is_copyable<trivial_mock_typeable>::value);
+        REQUIRE(type::has_trivial_copy<trivial_mock_typeable>::value);
+    }
+
+    SECTION("destroy")
+    {
+        REQUIRE(!type::is_destructible<typeable<mock_type>>::value);
+        REQUIRE(!type::has_trivial_destructor<typeable<mock_type>>::value);
+
+        REQUIRE(type::is_destructible<mock_typeable>::value);
+        REQUIRE(type::has_trivial_destructor<mock_typeable>::value);
+
+        REQUIRE(!type::is_destructible<typeable<uint32>>::value);
+        REQUIRE(!type::has_trivial_destructor<typeable<uint32>>::value);
+
+        REQUIRE(type::is_destructible<trivial_mock_typeable>::value);
+        REQUIRE(type::has_trivial_destructor<trivial_mock_typeable>::value);
+    }
+
+    SECTION("equality")
+    {
+        REQUIRE(!type::has_equality<typeable<mock_type>>::value);
+        REQUIRE(!type::has_equality<mock_typeable>::value);
+        REQUIRE(!type::has_equality<typeable<uint32>>::value);
+        REQUIRE(!type::has_equality<trivial_mock_typeable>::value);
+    }
+
+    SECTION("pod")
+    {
+        REQUIRE(!type::is_pod<typeable<mock_type>>::value);
+        REQUIRE(!type::is_pod<mock_typeable>::value);
+        REQUIRE(type::is_pod<typeable<uint32>>::value);
+        REQUIRE(type::is_pod<trivial_mock_typeable>::value);
+    }
+
+    SECTION("polymorphic")
+    {
+        REQUIRE(!type::is_polymorphic<typeable<mock_type>>::value);
+        REQUIRE(!type::is_polymorphic<mock_typeable>::value);
+        REQUIRE(!type::is_polymorphic<typeable<uint32>>::value);
+        REQUIRE(!type::is_polymorphic<trivial_mock_typeable>::value);
+    }
+
+    SECTION("sizeof")
+    {
+        REQUIRE(
+            sizeof(typeable<mock_type>) ==
+            sizeof(typeable<mock_type>::item_type));
+        REQUIRE(
+            sizeof(mock_typeable) ==
+            sizeof(mock_typeable::item_type));
+        REQUIRE(
+            sizeof(typeable<uint32>) ==
+            sizeof(typeable<uint32>::item_type));
+        REQUIRE(
+            sizeof(trivial_mock_typeable) ==
+            sizeof(trivial_mock_typeable::item_type));
+    }
+}
+
+TEST_CASE("typeable.constructor", "[gg.typeable]")
+{
+    SECTION("typeable")
+    {
+        mock_typeable typeable;
+        REQUIRE(typeable.is_type(0));
+    }
+
+    SECTION("typeable(type)")
+    {
+        mock_type type(123);
+        mock_typeable typeable(type);
+        REQUIRE(typeable.is_type(type));
+        REQUIRE(type == 123);
+    }
+
+    SECTION("typeable(rvalue_type_type)")
+    {
+        mock_type type(123);
+        mock_type moved_type(type);
+        mock_typeable typeable(type::move(moved_type));
+        REQUIRE(typeable.is_type(type));
+        REQUIRE(moved_type == 0);
+        REQUIRE(type == 123);
+    }
+
+    SECTION("typeable(typeable)")
+    {
+        mock_type type(123);
+        mock_typeable copied_typeable(type);
+        mock_typeable typeable(copied_typeable);
+        REQUIRE(typeable.is_type(copied_typeable.get_type()));
+    }
+
+    SECTION("typeable(rvalue_typeable)")
+    {
+        mock_typeable moved_typeable(123);
+        mock_typeable copied_typeable(moved_typeable);
+        mock_typeable typeable(type::move(moved_typeable));
+        REQUIRE(typeable.is_type(copied_typeable.get_type()));
+        REQUIRE(moved_typeable.is_type(0));
+    }
+}
+
+TEST_CASE("typeable.get_type", "[gg.typeable]")
+{
+    SECTION("get_type")
+    {
+        mock_typeable typeable(123);
+        REQUIRE(typeable.get_type() == 123);
+    }
+}
+
+TEST_CASE("typeable.set_type", "[gg.typeable]")
+{
+    SECTION("set_type(type)")
+    {
+        mock_type type(123);
+        mock_typeable typeable(0);
+        typeable.set_type(type);
+        REQUIRE(typeable.is_type(type));
+        REQUIRE(type == 123);
+    }
+
+    SECTION("set_type(rvalue_type_type)")
+    {
+        mock_type type(123);
+        mock_type moved_type(type);
+        mock_typeable typeable(0);
+        typeable.set_type(type::move(moved_type));
+        REQUIRE(typeable.is_type(type));
+        REQUIRE(moved_type == 0);
+        REQUIRE(type == 123);
+    }
+}
+
+TEST_CASE("typeable.is_type", "[gg.typeable]")
+{
+    SECTION("is_type")
+    {
+        mock_typeable typeable(0);
+        typeable.set_type(123);
+        REQUIRE(typeable.is_type(123));
+    }
+
+    SECTION("!is_type")
+    {
+        mock_typeable typeable(0);
+        typeable.set_type(123);
+        REQUIRE(!typeable.is_type(321));
+    }
+}
+
+//==============================================================================
+}
+//==============================================================================
