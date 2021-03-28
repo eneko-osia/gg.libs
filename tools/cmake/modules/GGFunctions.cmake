5f594b0ebb46be3d611d129fa3d3dd07abb4a7a8
eneko-osia <eneko.osia@gmail.com>
2021-03-28 11:58:34 +0200

    [cmake] remove python dependency

--- tools/cmake/modules/GGFunctions.cmake
+++ tools/cmake/modules/GGFunctions.cmake
@@ -10,3 +10,42 @@ function(create_groups base_dir files_to_group)
 endfunction()
 
 #===============================================================================
+
+function(generate_src_files base_dir)
+    set(filename "${base_dir}/GGSrcFiles.cmake")
+    file(WRITE "${filename}" "# @generated - run cmake configure to generate this file\n\n")
+
+    list(APPEND include_file_extensions "h" "hpp")
+    list(APPEND src_file_extensions "h" "hpp" "c" "cpp")
+    write_files_in_folder(${filename} ${base_dir} "include" "${include_file_extensions}")
+    write_files_in_folder(${filename} ${base_dir} "src" "${src_file_extensions}")
+    write_files_in_folder(${filename} ${base_dir} "test" "${src_file_extensions}")
+
+    file(APPEND "${filename}" "# groups\n")
+    file(APPEND "${filename}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${include_files}\")\n")
+    file(APPEND "${filename}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${src_files}\")\n")
+    file(APPEND "${filename}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${test_files}\")\n")
+endfunction()
+
+#===============================================================================
+
+function(write_files_in_folder filename base_dir directory extensions)
+    foreach(extension ${extensions})
+        list(APPEND globbing_expressions "${base_dir}/${directory}/*.${extension}")
+    endforeach()
+
+    file(GLOB_RECURSE files ${globbing_expressions})
+    if(NOT files STREQUAL "")
+        file(APPEND "${filename}" "# ${directory} files\n")
+        file(APPEND "${filename}" "set(${directory}_files\n")
+        foreach(file ${files})
+            string(REPLACE "${base_dir}" "\${CMAKE_CURRENT_SOURCE_DIR}" file "${file}")
+            file(APPEND "${filename}" "    \"${file}\"\n")
+        endforeach()
+        file(APPEND "${filename}" ")\n")
+    endif()
+endfunction()
+
+#===============================================================================
+
+#===============================================================================
