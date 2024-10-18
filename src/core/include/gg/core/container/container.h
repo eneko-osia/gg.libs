1e898e2c102ef066b4e520487c576c6fe90ceced
eneko-osia <eneko.osia@gmail.com>
2024-10-18 10:36:28 +0200

    [core] container updated

--- src/core/include/gg/core/container/container.h
+++ src/core/include/gg/core/container/container.h
@@ -14,27 +14,21 @@ namespace gg
         inline ITERATOR
         find_if(ITERATOR begin, ITERATOR end, FUNCTION && function) noexcept
         {
-            return
-                std::find_if(
-                    begin, end, type::forward<FUNCTION>(function));
+            return std::find_if(begin, end, type::forward<FUNCTION>(function));
         }
 
         template <typename ITERATOR, typename FUNCTION>
         inline FUNCTION
         for_each(ITERATOR begin, ITERATOR end, FUNCTION && function) noexcept
         {
-            return
-                std::for_each(
-                    begin, end, type::forward<FUNCTION>(function));
+            return std::for_each(begin, end, type::forward<FUNCTION>(function));
         }
 
         template <typename ITERATOR, typename FUNCTION>
         inline ITERATOR
         remove_if(ITERATOR begin, ITERATOR end, FUNCTION && function) noexcept
         {
-            return
-                std::remove_if(
-                    begin, end, type::forward<FUNCTION>(function));
+            return std::remove_if(begin, end, type::forward<FUNCTION>(function));
         }
 
         template <typename ITERATOR>
