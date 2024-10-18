e4e3dbcdbe7f7d2407f52c7e49ca0a02724b6aff
eneko-osia <eneko.osia@gmail.com>
2024-10-18 10:34:14 +0200

    [cmake] cmake files updated

--- tools/cmake/modules/GGFunctions.cmake
+++ tools/cmake/modules/GGFunctions.cmake
@@ -1,51 +1,50 @@
 #===============================================================================
 
-function(create_groups base_dir files_to_group)
-    foreach(file_to_group IN LISTS files_to_group)
-        get_filename_component(file_directory "${file_to_group}" DIRECTORY)
-        string(REPLACE "${base_dir}" "" group "${file_directory}")
-        string(REPLACE "/" "\\" group "${group}")
-        source_group("${group}" FILES "${file_to_group}")
+function(create_groups BASE_DIR FILES_TO_GROUP)
+    foreach(FILE_TO_GROUP IN LISTS FILES_TO_GROUP)
+        get_filename_component(FILE_DIRECTORY "${FILE_TO_GROUP}" DIRECTORY)
+        string(REPLACE "${BASE_DIR}" "" GROUP "${FILE_DIRECTORY}")
+        string(REPLACE "/" "\\" GROUP "${GROUP}")
+        source_group("${GROUP}" FILES "${FILE_TO_GROUP}")
     endforeach()
 endfunction()
 
 #===============================================================================
 
-function(generate_src_files base_dir)
-    set(filename "${base_dir}/GGSrcFiles.cmake")
-    file(WRITE "${filename}" "# @generated - run cmake configure to generate this file\n\n")
+function(generate_src_files BASE_DIR)
+    set(FILENAME "${BASE_DIR}/GGSrcFiles.cmake")
+    file(WRITE "${FILENAME}" "# @generated - run cmake configure to generate this file\n\n")
 
-    list(APPEND include_file_extensions "h" "hpp")
-    list(APPEND src_file_extensions "h" "hpp" "c" "cpp")
-    write_files_in_folder(${filename} ${base_dir} "include" "${include_file_extensions}")
-    write_files_in_folder(${filename} ${base_dir} "src" "${src_file_extensions}")
-    write_files_in_folder(${filename} ${base_dir} "test" "${src_file_extensions}")
+    list(APPEND INCLUDE_FILE_EXTENSIONS "h" "hpp")
+    list(APPEND SRC_FILE_EXTENSIONS "h" "hpp" "c" "cpp")
+    write_files_in_folder(${FILENAME} ${BASE_DIR} "include" "${INCLUDE_FILE_EXTENSIONS}")
+    write_files_in_folder(${FILENAME} ${BASE_DIR} "src" "${SRC_FILE_EXTENSIONS}")
+    write_files_in_folder(${FILENAME} ${BASE_DIR} "test" "${SRC_FILE_EXTENSIONS}")
 
-    file(APPEND "${filename}" "# groups\n")
-    file(APPEND "${filename}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${include_files}\")\n")
-    file(APPEND "${filename}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${src_files}\")\n")
-    file(APPEND "${filename}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${test_files}\")\n")
+    file(APPEND "${FILENAME}" "# groups\n")
+    file(APPEND "${FILENAME}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${INCLUDE_FILES}\")\n")
+    file(APPEND "${FILENAME}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${SRC_FILES}\")\n")
+    file(APPEND "${FILENAME}" "create_groups(\${CMAKE_CURRENT_SOURCE_DIR} \"\${TEST_FILES}\")\n")
 endfunction()
 
 #===============================================================================
 
-function(write_files_in_folder filename base_dir directory extensions)
-    foreach(extension ${extensions})
-        list(APPEND globbing_expressions "${base_dir}/${directory}/*.${extension}")
+function(write_files_in_folder FILENAME BASE_DIR DIRECTORY EXTENSIONS)
+    foreach(EXTENSION ${EXTENSIONS})
+        list(APPEND GLOBBING_EXPRESSIONS "${BASE_DIR}/${DIRECTORY}/*.${EXTENSION}")
     endforeach()
 
-    file(GLOB_RECURSE files ${globbing_expressions})
-    if(NOT files STREQUAL "")
-        file(APPEND "${filename}" "# ${directory} files\n")
-        file(APPEND "${filename}" "set(${directory}_files\n")
-        foreach(file ${files})
-            string(REPLACE "${base_dir}" "\${CMAKE_CURRENT_SOURCE_DIR}" file "${file}")
-            file(APPEND "${filename}" "    \"${file}\"\n")
+    file(GLOB_RECURSE FILES ${GLOBBING_EXPRESSIONS})
+    if(NOT FILES STREQUAL "")
+        string(TOUPPER ${DIRECTORY} DIRECTORY_UPPER)
+        file(APPEND "${FILENAME}" "# ${DIRECTORY} files\n")
+        file(APPEND "${FILENAME}" "set(${DIRECTORY_UPPER}_FILES\n")
+        foreach(FILE ${FILES})
+            string(REPLACE "${BASE_DIR}" "\${CMAKE_CURRENT_SOURCE_DIR}" FILE "${FILE}")
+            file(APPEND "${FILENAME}" "    \"${FILE}\"\n")
         endforeach()
-        file(APPEND "${filename}" ")\n")
+        file(APPEND "${FILENAME}" ")\n")
     endif()
 endfunction()
-
-#===============================================================================
 
 #===============================================================================
---
+++ tools/cmake/modules/GGSystem.cmake
@@ -0,0 +1,109 @@
+#===============================================================================
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    set(COMPILER "clang")
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    set(COMPILER "clang")
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(COMPILER "gnu")
+elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    set(COMPILER "msvc")
+else()
+    message(FATAL_ERROR "Unsuported c++ compiler [${CMAKE_CXX_COMPILER_ID}]")
+endif()
+
+#===============================================================================
+
+if(CMAKE_SYSTEM_NAME STREQUAL "Android")
+    set(SYSTEM "android")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    set(SYSTEM "mac")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    set(SYSTEM "linux")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    set(SYSTEM "windows")
+elseif(CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    set(SYSTEM "uwp")
+else()
+    message(FATAL_ERROR "Unsuported build system [${CMAKE_SYSTEM_NAME}]")
+endif()
+
+#===============================================================================
+
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(SYSTEM_ARCHITECTURE "x86_64")
+else()
+    set(SYSTEM_ARCHITECTURE "x86")
+endif()
+
+#===============================================================================
+
+set(CMAKE_CONFIGURATION_TYPES "debug" "release_with_debug" "release"
+    CACHE STRING "Build configuration types" FORCE
+)
+mark_as_advanced(CMAKE_CONFIGURATION_TYPES)
+
+#===============================================================================
+
+set(CMAKE_C_FLAGS_RELEASE_WITH_DEBUG ${CMAKE_C_FLAGS_RELWITHDEBINFO}
+    CACHE STRING "c release with debug compiler flags"
+)
+mark_as_advanced(CMAKE_C_FLAGS_RELEASE_WITH_DEBUG)
+
+set(CMAKE_CXX_FLAGS_RELEASE_WITH_DEBUG ${CMAKE_CXX_FLAGS_RELWITHDEBINFO}
+    CACHE STRING "cxx release with debug compiler flags"
+)
+mark_as_advanced(CMAKE_CXX_FLAGS_RELEASE_WITH_DEBUG)
+
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE_WITH_DEBUG ${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO}
+    CACHE STRING "cxx release with debug linker flags"
+)
+mark_as_advanced(CMAKE_EXE_LINKER_FLAGS_RELEASE_WITH_DEBUG)
+
+set(CMAKE_MODULE_LINKER_FLAGS_RELEASE_WITH_DEBUG ${CMAKE_MODULE_LINKER_FLAGS_RELWITHDEBINFO}
+    CACHE STRING "cxx release with debug module linker flags"
+)
+mark_as_advanced(CMAKE_MODULE_LINKER_FLAGS_RELEASE_WITH_DEBUG)
+
+set(CMAKE_SHARED_LINKER_FLAGS_RELEASE_WITH_DEBUG ${CMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO}
+    CACHE STRING "cxx release with debug shared linker flags"
+)
+mark_as_advanced(CMAKE_SHARED_LINKER_FLAGS_RELEASE_WITH_DEBUG)
+
+set(CMAKE_STATIC_LINKER_FLAGS_RELEASE_WITH_DEBUG ${CMAKE_STATIC_LINKER_FLAGS_RELWITHDEBINFO}
+    CACHE STRING "cxx release with debug static linker flags"
+)
+mark_as_advanced(CMAKE_STATIC_LINKER_FLAGS_RELEASE_WITH_DEBUG)
+
+#===============================================================================
+
+foreach(CONFIGURATION ${CMAKE_CONFIGURATION_TYPES})
+    string(TOUPPER ${CONFIGURATION} CONFIGURATION_UPPER)
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${CONFIGURATION_UPPER}
+        "${CMAKE_BINARY_DIR}/bin/${SYSTEM}/${SYSTEM_ARCHITECTURE}/${CONFIGURATION}"
+        CACHE STRING "Archive output directory [${CONFIGURATION}]"
+    )
+    mark_as_advanced(CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${CONFIGURATION_UPPER})
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_${CONFIGURATION_UPPER}
+        "${CMAKE_BINARY_DIR}/bin/${SYSTEM}/${SYSTEM_ARCHITECTURE}/${CONFIGURATION}"
+        CACHE STRING "Library output directory [${CONFIGURATION}]"
+    )
+    mark_as_advanced(CMAKE_LIBRARY_OUTPUT_DIRECTORY_${CONFIGURATION_UPPER})
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY_${CONFIGURATION_UPPER}
+        "${CMAKE_BINARY_DIR}/bin/${SYSTEM}/${SYSTEM_ARCHITECTURE}/${CONFIGURATION}"
+        CACHE STRING "Runtime output directory [${CONFIGURATION}]"
+    )
+    mark_as_advanced(CMAKE_RUNTIME_OUTPUT_DIRECTORY_${CONFIGURATION_UPPER})
+endforeach()
+
+#===============================================================================
+
+set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${CMAKE_CONFIGURATION_TYPES})
+if(NOT(CMAKE_BUILD_TYPE STREQUAL "debug" OR
+       CMAKE_BUILD_TYPE STREQUAL "release_with_debug" OR
+       CMAKE_BUILD_TYPE STREQUAL "release"))
+    set(CMAKE_BUILD_TYPE "debug" CACHE STRING "Build type [${CMAKE_CONFIGURATION_TYPES}]" FORCE)
+endif()
+
+#===============================================================================
+
